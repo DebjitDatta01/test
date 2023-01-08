c0eed37f52dd5f60f580823aa60ff0a9fc7ef396
unknown <debjit.datta.6901@gmail.com>
2023-01-08 14:59:34 +0530

    Revert "Error"
    This reverts commit 403c6a3c2bb404d110529857537a34dbaf23e4df.
    
    ok

--- practice.java
+++ practice.java
@@ -2,7 +2,6 @@ public class practice {
     public static void main(String[] args) {
         System.out.println("Practice");
         System.out.println("HEllo Debjit");
-        asdhgkdsjghdskg
     }
 
 }
