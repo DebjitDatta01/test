9acda085fc4d48a7809261a083bd0c05bac41f88
unknown <debjit.datta.6901@gmail.com>
2023-01-08 14:47:53 +0530

    Hello

--- practice.java
+++ practice.java
@@ -2,9 +2,6 @@ public class practice {
     public static void main(String[] args) {
         System.out.println("Practice");
         System.out.println("HEllo Debjit");
-        System.out.println("Practice");
-        System.out.println("HEllo Debjit");
-
     }
 
 }
