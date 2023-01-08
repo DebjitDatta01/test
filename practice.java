6139dbcb9cf04643a176a90e1dcbdfc620001243
unknown <debjit.datta.6901@gmail.com>
2023-01-08 14:54:08 +0530

    Error fixed

--- practice.java
+++ practice.java
@@ -2,7 +2,6 @@ public class practice {
     public static void main(String[] args) {
         System.out.println("Practice");
         System.out.println("HEllo Debjit");
-        96f77d4490ab5559b8deec7087cb5d20da968350
     }
 
 }
