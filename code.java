c520377f2f60e8d04013d8eaeb8d225c11ed054a
unknown <debjit.datta.6901@gmail.com>
2023-01-21 21:25:28 +0530

    Thirsty branch

--- code.java
+++ code.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class code {
 
-public static void main(String [] args){
-	Scanner sc = new Scanner(System.in);
-	int x = sc.nextInt();
-	int y = sc.nextInt();
-	
-	System.out.println("the sum is "+ (x+ y));
-}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+
+		System.out.println("the sum is " + (x + y));
+
+		System.out.println("To check parallel coding ");
+
+	}
 
 }
